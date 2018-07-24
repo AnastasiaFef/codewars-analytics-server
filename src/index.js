732ad8a6b587809adb2c187df09297ad1211d325
Viktor Bogutskii <info@siteograf.com>
2018-07-23 19:59:59 -0700

    index.js and message

---
+++ src/index.js
@@ -0,0 +1,79 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+// import userRouter from './modules/user/userRoutes';
+
+
+import message from './modules/messages/messages';
+
+const PORT = +process.env.PORT || 5000;
+const app = express();
+
+// ===== DISABLE EXPRESS SIGNATURE ======
+app.disable('x-powered-by');
+
+// ===== DATABASE ======
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${
+    process.env.MONGO_ATLAS_PWD
+  }@${process.env.MONGO_ATLAS_HOST}/${process.env.MONGO_ATLAS_DB_NAME}`,
+);
+
+mongoose.connection.on('error', () => {
+  throw new Error('Unable to connect to database');
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose default connection disconnected');
+});
+
+// ===== LOGGER =====
+app.use(morgan('dev'));
+
+// ===== PARSE RESPONSE =====
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+app.use(bodyParser.json()); // support json encoded bodies
+
+// ===== CORS =====
+app.use((req, res, next) => {
+  // eslint-disable-line consistent-return
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// ===== IGNORE FAVICON =====
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+// ===== ROUTING =====
+// app.use('/user', userRouter);
+// app.use('/product', productRouter);
+// app.use('/vocabular', vocabularRouter);
+// app.use('/file', fileRouter);
+// app.use('/backup', backupRouter);
+// app.use('/offer', offertRouter);
+
+// ===== ERROR HANDLING =====
+app.use((req, res, next) =>
+  res.status(404).json(message.error('API not found')),
+); // eslint-disable-line no-unused-vars
+
+app.use((error, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  res.status(error.status || 500);
+  res.json(message.error(error.message));
+});
+
+// ===== PORT =====
+app.listen(PORT, () => {
+  console.log(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+});
---
+++ src/modules/messages/messages.js
@@ -0,0 +1,19 @@
+const message = {};
+
+message.success = (text, payload) => ({
+  message: {
+    text,
+    type: 'success'
+  },
+  payload
+});
+
+message.error = (text, payload) => ({
+  message: {
+    text,
+    type: 'error'
+  },
+  payload
+});
+
+export default message;
