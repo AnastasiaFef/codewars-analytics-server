5bb9394dedc85347bd3c5450b44dc62466d5612e
Viktor Bogutskii <info@siteograf.com>
2018-08-17 13:51:46 -0700

    user model update

--- src/modules/user/userModel.js
+++ src/modules/user/userModel.js
@@ -1,25 +1,28 @@
 import mongoose from 'mongoose';
 
-const userSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+const userSchema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    codewarsId: {
+      type: String,
+      required: true,
+    },
+    codewarsAnalytics: {
+      type: Array,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  codewarsId: {
-    type: String,
-    required: true,
-  },
-  codewarsAnalytics: {
-    type: Array,
-    required: false,
-  },
-});
+  { timestamps: {} },
+);
 
 export default mongoose.model('User', userSchema);
