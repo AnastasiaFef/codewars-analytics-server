86df3098dabb4e0358bf59f65d8299a70116b67c
Viktor Bogutskii <info@siteograf.com>
2018-09-28 13:11:57 -0700

    fixes

--- src/modules/info/infoControllers.js
+++ src/modules/info/infoControllers.js
@@ -1,15 +1,14 @@
 import message from '../messages/messages';
 
 export const info = (req, res) => {
-  const now = new Date()
+  const now = new Date();
 
   const message = {
     timestamp: now.getTime(),
     localeString: now.toLocaleString(),
     getTimezoneOffset: now.getTimezoneOffset(),
-    tz: process.env.TZ
-  }
+    tz: process.env.TZ,
+  };
 
   res.status(200).json(message);
 };
-
