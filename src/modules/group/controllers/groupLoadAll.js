f83a8678f6f65e0161b61a9fde16c3b7e8a95f00
Viktor Bogutskii <info@siteograf.com>
2018-10-04 15:15:52 -0700

    add user name to group

--- src/modules/group/controllers/groupLoadAll.js
+++ src/modules/group/controllers/groupLoadAll.js
@@ -1,15 +1,21 @@
-import Group from './../groupModel';
+import Group from '../groupModel';
 import message from '../../messages/messages';
 
 const groupLoadAll = (req, res) => {
   Group.find()
+    .populate({
+      path: 'members',
+      // match: { age: { $gte: 21 } },
+      // Explicitly exclude `_id`, see http://bit.ly/2aEfTdB
+      select: 'codewarsId -_id',
+      // options: { limit: 5 },
+    })
     .select('-__v')
     .exec()
     .then(docs => {
       res.status(200).json(docs);
     })
     .catch(err => {
-      console.log(err);
       res.status(500).json(message.error(err));
     });
 };
