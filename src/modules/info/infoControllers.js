c3ef6e801a8e5c2c185be5f32c7bf3a49b79cef7
Viktor Bogutskii <info@siteograf.com>
2018-10-12 10:02:58 -0700

    include version to info

--- src/modules/info/infoControllers.js
+++ src/modules/info/infoControllers.js
@@ -1,5 +1,3 @@
-import message from '../messages/messages';
-
 export const info = (req, res) => {
   const now = new Date();
 
@@ -8,6 +6,7 @@ export const info = (req, res) => {
     localeString: now.toLocaleString(),
     getTimezoneOffset: now.getTimezoneOffset(),
     tz: process.env.TZ,
+    name: 'Progress Monitor v0.1.0',
   };
 
   res.status(200).json(message);
